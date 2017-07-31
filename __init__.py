02fff109a73d0ab69a5dcb2607786005c6d08e3e
Gyro Gearloose <scholar0x0100@gmail.com>
2017-07-31 14:39:57 +0200

    added menu operator for hudgun import

--- __init__.py
+++ __init__.py
@@ -33,13 +33,23 @@ if "bpy" in locals():
 	import importlib
 	importlib.reload(sys.modules['md5.io_md5mesh'])
 	importlib.reload(sys.modules['md5.io_md5anim'])
+	importlib.reload(sys.modules['md5.import_hudguns'])
 
+import os
 import bpy
-from bpy.props import StringProperty, IntProperty
+from bpy.props import StringProperty, IntProperty, FloatProperty, EnumProperty, CollectionProperty
 from bpy_extras.io_utils import ImportHelper, ExportHelper
 
 from .io_md5mesh import read_md5mesh, write_md5mesh
 from .io_md5anim import read_md5anim, write_md5anim
+from .import_hudguns import setup_cam, import_from_config
+
+class MD5Preferences(bpy.types.AddonPreferences):
+	bl_idname = __name__
+	sb_dir = StringProperty(name="Sauerbraten Directory", default="", subtype="DIR_PATH")
+
+	def draw(self, context):
+		self.layout.prop(self, "sb_dir")
 
 class OT_IMPORT_MESH_md5mesh(bpy.types.Operator, ImportHelper):
 	bl_idname = "import_mesh.md5"
@@ -109,13 +119,82 @@ class OT_EXPORT_ANIM_md5anim(bpy.types.Operator, ExportHelper):
 		write_md5anim(self.filepath, context.scene, context.active_object, self.bone_layer)
 		return {"FINISHED"}
 
+
+class OT_IMPORT_MESH_sb_hudgun(bpy.types.Operator, ImportHelper):
+	bl_idname = "import_mesh.sb_hudgun"
+	bl_label = "Import Sauerbraten Hudgun"
+	bl_options = {"REGISTER", "UNDO"}
+
+	filename_ext = ".cfg"
+	filter_glob = StringProperty(default="*.cfg", options={'HIDDEN'})
+	playermodel = EnumProperty(items=(
+		("MRFIXIT",       "Mr Fixit",      "", 0),
+		("SNOUTX10K",     "SnoutX10K",     "", 1),
+		("INKY",          "Inky",          "", 2),
+		("CAPTAINCANNON", "CaptainCannon", "", 3)),
+		name="Playermodel",
+		default="SNOUTX10K"
+	)
+
+	weapon = EnumProperty(items=(
+		("SHOTG",  "Shotgun",         "", 1),
+		("CHAING", "Chaingun",        "", 2),
+		("ROCKET", "Rocketlauncher",  "", 3),
+		("RIFLE",  "Rifle",           "", 4),
+		("GL",     "Grenadelauncher", "", 5),
+		("PISTOL", "Pistol",          "", 6)),
+		name="Weapon",
+		default="RIFLE"
+	)
+
+	fov = FloatProperty(name="Field Of View", default=65.0, subtype="NONE")
+	res_x = IntProperty(name="Window Width",  default=800,  subtype="PIXEL")
+	res_y = IntProperty(name="Window Height", default=600,  subtype="PIXEL")
+	directory = StringProperty(name="Directory", subtype="DIR_PATH",  description="Directory of the file")
+	filename  = StringProperty(name="Filename",  subtype="FILE_NAME", description="Name of the file", default="", options={'SKIP_SAVE', 'TEXTEDIT_UPDATE'})
+	# filesel = CollectionProperty(name="Files", type=bpy.types.OperatorFileListElement)
+
+	def execute(self, context):
+		pref = context.user_preferences.addons[__name__].preferences
+
+		if self.filename == "":
+			if pref.sb_dir == "":
+				pref.sb_dir = self.directory
+			self.filepath = os.path.join(
+				pref.sb_dir, "packages", "models",
+				self.playermodel.lower(), "hudguns", self.weapon.lower(), "md5.cfg")
+
+		setup_cam(context.scene, self.fov, self.res_x, self.res_y)
+		import_from_config(pref.sb_dir, self.filepath, self.weapon.lower())
+		context.scene.update()
+		context.scene.frame_set(0)
+		return {"FINISHED"}
+
+	def draw(self, context):
+		pref = context.user_preferences.addons[__name__].preferences
+		layout = self.layout
+		if not context.space_data.params.filename == "":
+			layout.label(text="File selected - Dropdown will be ignored", icon="QUESTION")
+			enabled = False
+		else:
+			enabled = True
+		col = layout.column()
+		col.enabled = enabled
+		col.prop(self, "playermodel")
+		col.prop(self, "weapon")
+		layout.prop(self, "fov")
+		layout.prop(self, "res_x")
+		layout.prop(self, "res_y")
+		layout.prop(pref, "sb_dir")
+
 reg_table = (
-	[OT_IMPORT_MESH_md5mesh, bpy.types.INFO_MT_file_import, "MD5 Mesh (.md5mesh)"	  ],
-	[OT_IMPORT_ANIM_md5anim, bpy.types.INFO_MT_file_import, "MD5 Animation (.md5anim)"],
-	[OT_EXPORT_MESH_md5mesh, bpy.types.INFO_MT_file_export, "MD5 Mesh (.md5mesh)"     ],
-	[OT_EXPORT_ANIM_md5anim, bpy.types.INFO_MT_file_export, "MD5 Animation (.md5anim)"]
+	[OT_IMPORT_MESH_md5mesh,   bpy.types.INFO_MT_file_import, "MD5 Mesh (.md5mesh)"	   ],
+	[OT_IMPORT_ANIM_md5anim,   bpy.types.INFO_MT_file_import, "MD5 Animation (.md5anim)"],
+	[OT_EXPORT_MESH_md5mesh,   bpy.types.INFO_MT_file_export, "MD5 Mesh (.md5mesh)"     ],
+	[OT_EXPORT_ANIM_md5anim,   bpy.types.INFO_MT_file_export, "MD5 Animation (.md5anim)"],
+	[OT_IMPORT_MESH_sb_hudgun, bpy.types.INFO_MT_file_import, "Sauerbraten Hudgun (.cfg)"]
 )
-	
+
 def generate_menu_function(op_cls, description):
 	def mnu_func(self, context):
 		self.layout.operator(op_cls.bl_idname, text=description)
@@ -125,11 +204,13 @@ for row in reg_table:
 	row[2] = generate_menu_function(row[0], row[2])
 
 def register():
+	bpy.utils.register_class(MD5Preferences)
 	for cls, mnu, mnu_func in reg_table:
 		bpy.utils.register_class(cls)
 		mnu.append(mnu_func)
 
 def unregister():
+	bpy.utils.unregister_class(MD5Preferences)
 	for cls, mnu, mnu_func in reg_table:
 		mnu.remove(mnu_func)
 		bpy.utils.unregister_class(cls)
